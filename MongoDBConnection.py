515286b5c79c8e5a99951b96e00263a75ebf9a4b
maxi <maxiguillermo@gmail.com>
2024-04-23 16:45:23 -0700

    initial commit

---
+++ MongoDBConnection.py
@@ -0,0 +1,52 @@
+from pymongo import MongoClient, database
+import subprocess
+import threading
+import pymongo
+from datetime import datetime, timedelta
+import time
+
+DBName = "test" #Use this to change which Database we're accessing
+connectionURL = "[DatabaseURL]@[Database]" #Put your database URL here
+sensorTable = "sensor data table" #Change this to the name of your sensor data table
+
+def QueryToList(query):
+  pass; #TODO: Convert the query that you get in this function to a list and return it
+  #HINT: MongoDB queries are iterable
+
+def QueryDatabase() -> []:
+	global DBName
+	global connectionURL
+	global currentDBName
+	global running
+	global filterTime
+	global sensorTable
+	cluster = None
+	client = None
+	db = None
+	try:
+		cluster = connectionURL
+		client = MongoClient(cluster)
+		db = client[DBName]
+		print("Database collections: ", db.list_collection_names())
+
+		#We first ask the user which collection they'd like to draw from.
+		sensorTable = db[sensorTable]
+		print("Table:", sensorTable)
+		#We convert the cursor that mongo gives us to a list for easier iteration.
+		timeCutOff = datetime.now() - timedelta(minutes=0) #TODO: Set how many minutes you allow
+
+		oldDocuments = QueryToList(sensorTable.find({"time":{"$gte":timeCutOff}}))
+		currentDocuments = QueryToList(sensorTable.find({"time":{"$lte":timeCutOff}}))
+
+		print("Current Docs:",currentDocuments)
+		print("Old Docs:",oldDocuments)
+
+		#TODO: Parse the documents that you get back for the sensor data that you need
+		#Return that sensor data as a list
+
+
+	except Exception as e:
+		print("Please make sure that this machine's IP has access to MongoDB.")
+		print("Error:",e)
+		exit(0)
+
---
+++ Server.py
@@ -0,0 +1,68 @@
+import socket
+import ipaddress
+import threading
+import time
+import contextlib
+import errno
+from dataclasses import dataclass
+import random
+import sys
+
+maxPacketSize = 1024
+defaultPort = 0 #TODO: Set this to your preferred port
+
+def GetFreePort(minPort: int = 1024, maxPort: int = 65535):
+    for i in range(minPort, maxPort):
+        print("Testing port",i);
+        with contextlib.closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as potentialPort:
+            try:
+                potentialPort.bind(('localhost', i));
+                potentialPort.close();
+                print("Server listening on port",i);
+                return i
+            except socket.error as e:
+                if e.errno == errno.EADDRINUSE:
+                    print("Port",i,"already in use. Checking next...");
+                else:
+                    print("An exotic error occurred:",e);
+
+def GetServerData() -> []:
+    import MongoDBConnection as mongo
+    return mongo.QueryDatabase();
+
+
+
+
+
+
+def ListenOnTCP(tcpSocket: socket.socket, socketAddress):
+    pass; #TODO: Implement TCP Code, use GetServerData to query the database.
+
+
+
+
+
+
+
+def CreateTCPSocket() -> socket.socket:
+    tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+    tcpPort = defaultPort
+    print("TCP Port:",tcpPort);
+    tcpSocket.bind(('localhost', tcpPort));
+    return tcpSocket;
+
+def LaunchTCPThreads():
+    tcpSocket = CreateTCPSocket();
+    tcpSocket.listen(5);
+    while True:
+        connectionSocket, connectionAddress = tcpSocket.accept();
+        connectionThread = threading.Thread(target=ListenOnTCP, args=[connectionSocket, connectionAddress]);
+        connectionThread.start();
+
+if __name__ == "__main__":
+    tcpThread = threading.Thread(target=LaunchTCPThreads);
+    tcpThread.start();
+
+    while not exitSignal:
+        time.sleep(1);
+    print("Ending program by exit signal...");
